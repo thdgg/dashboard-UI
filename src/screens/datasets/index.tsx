3f9647e8b8684d9375ada7e3687bb095416c8c2b
Vivarium69420 <vivarium69420@gmail.com>
2023-12-24 14:01:33 +0700

    testing navbar

--- src/screens/datasets/index.tsx
+++ src/screens/datasets/index.tsx
@@ -1,5 +1,16 @@
+import Navbar from "../navbar";
+
 const Datasets = () => {
-  return <div>Datasets</div>;
+  return (
+    <div className="relative">
+      <div className="absolute top-0 left-0">
+        <Navbar />
+      </div>
+      <div className="ml-28">
+
+      </div>
+    </div>
+  );
 };
 
 export default Datasets;

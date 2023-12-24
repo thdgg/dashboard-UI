b6b7fff4ee3dc7124340cd1b3993f5ddb63ad05d
Vivarium69420 <vivarium69420@gmail.com>
2023-12-24 16:23:10 +0700

    layout for searchbar

---
+++ src/components/searchbar/index.tsx
@@ -0,0 +1,19 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+
+const Searchbar = () => {
+  return (
+        <div className='w-3/4 p-2 mx-auto my-auto border-2 rounded-full'>
+            <div className="relative flex items-center w-full h-12 rounded-full focus-within:shadow-lg bg-white overflow-hidden">
+                <div className="grid place-items-center h-full w-12">
+                    <MagnifyingGlassIcon className="h-6 w-6" />
+                </div>
+                <input className="peer h-full w-full outline-none text-lg text-gray-700 pr-2"
+                type="text"
+                id="search"
+                placeholder="Search something.." /> 
+             </div>
+        </div>
+  )
+}
+
+export default Searchbar

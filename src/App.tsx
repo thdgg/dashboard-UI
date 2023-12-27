0477f6745ec5076321f748564dab37fe869033a0
Vivarium69420 <vivarium69420@gmail.com>
2023-12-27 18:25:26 +0700

    move routing logic to App to use authentication routing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import Navbar from "./screens/navbar";
+import Explorer from "./screens/explorer/index.tsx";
+import Models from "./screens/models/index.tsx";
+import Test from "./screens/test/index.tsx";
+import Datasets from "./screens/datasets/index.tsx";
+import Home from "./screens/home/index.tsx";
+import Authenticate from "./screens/authenticate/index.tsx";
+import useAuth from "./hooks/useAuth.tsx";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-function App() {
-  return <Navbar />;
-}
+const App = () => {
+  const {auth} = useAuth();
+  const navigate = useNavigate();
+  console.log(auth);
+
+  useEffect(() => {
+    if (!auth) {
+      navigate("/authenticate");
+    }
+    else {
+      navigate("/home");
+    }
+  }, [auth]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {auth ? (
+        <>
+          <Route path="/home" element={<Home />} />
+          <Route path="/explorer" element={<Explorer />} />
+          <Route path="/datasets" element={<Datasets />} />
+          <Route path="/models" element={<Models />} />
+          <Route path="tests" element={<Test />} />
+        </>
+      ) : (
+        <Route path="/authenticate" element={<Authenticate />} />
+      )}
+    </Routes>
+  );
+};
+
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Explorer from "./screens/explorer/index.tsx";
-import Models from "./screens/models/index.tsx";
-import Test from "./screens/test/index.tsx";
-import Datasets from "./screens/datasets/index.tsx";
-import Home from "./screens/home/index.tsx";
+import App from "./App.tsx";
+import AuthProvider from "./providers/AuthProvider.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/explorer" element={<Explorer />} />
-        <Route path="/datasets" element={<Datasets />} />
-        <Route path="/models" element={<Models />} />
-        <Route path="tests" element={<Test />} />
-      </Routes>
+    <AuthProvider>
+    <App />
+    </AuthProvider>
     </BrowserRouter>
+    
+    
   </React.StrictMode>
 );

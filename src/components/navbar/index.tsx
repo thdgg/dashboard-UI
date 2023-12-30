027dd13df93caf76e87ce4cc63429ab6d497fbf5
Vivarium69420 <vivarium69420@gmail.com>
2023-12-30 11:01:22 +0700

    fixing layout for subbox and navbar so it doesn't take up the screen

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -15,7 +15,9 @@ const Navbar = React.memo(() => {
   const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
   const [selectedMenuItem, setSelectedMenuItem] = useState<number>(0);
   const [isAddMenuVisible, setIsAddMenuVisible] = useState<boolean>(false);
-  const [isNewModelWindowVisible, setIsNewModelWindowVisible] = useState<boolean>(false);
+  const [isNewModelWindowVisible, setIsNewModelWindowVisible] = useState<
+    boolean
+  >(false);
 
   const location = useLocation();
   const isAboveMedium = useMediaQuery({ query: "(min-width: 768px)" });
@@ -55,131 +57,134 @@ const Navbar = React.memo(() => {
   }, [location]);
 
   return (
-    <div className="w-screen h-screen">
-     {
-        isNewModelWindowVisible && (
-          <SubmissionBox setIsNewModelWindowVisible={setIsNewModelWindowVisible} />
-        )
-     }
-    
-    <aside className="h-screen" ref={navbarRef}>
-      <nav className={`${isAboveMedium ? "w-full" : "w-0"} h-full`}>
-        {isAboveMedium || isMenuToggle
-          ? (
-            <div
-              className={` ${
-                isMenuToggle ? "w-64" : "w-20"
-              } flex flex-col p-2 h-full bg-white border-r shadow-sm ease-in-out`}
-            >
-              {/* TOGGLE AND LOGO */}
-              <div className={`${flexBetween} w-64 p-2`}>
+    <div>
+      {isNewModelWindowVisible && (
+        <SubmissionBox
+          setIsNewModelWindowVisible={setIsNewModelWindowVisible}
+        />
+      )}
+
+      <aside className="h-screen" ref={navbarRef}>
+        <nav className={`${isAboveMedium ? "w-full" : "w-0"} h-full`}>
+          {isAboveMedium || isMenuToggle
+            ? (
+              <div
+                className={` ${
+                  isMenuToggle ? "w-64" : "w-20"
+                } flex flex-col p-2 h-full bg-white border-r shadow-sm ease-in-out`}
+              >
+                {/* TOGGLE AND LOGO */}
+                <div className={`${flexBetween} w-64 p-2`}>
+                  <button>
+                    <Bars3Icon
+                      className="h-12 w-12 mr-4 ml-0 p-2 bg-grey-50 hover:bg-gray-100 rounded-full"
+                      onClick={() => setIsMenuToggle(!isMenuToggle)}
+                    />
+                  </button>
+                  <img
+                    src="https://img.logoipsum.com/243.svg"
+                    className={`${isMenuToggle ? "w-32" : "w-0"} duration-75`}
+                  />
+                </div>
+                {/* ADD BUTTON */}
+                <div
+                  className={`${flexBetween} ${
+                    isMenuToggle ? "w-32" : "w-16"
+                  } mb-3 p-2 border rounded-full bg-grey-50 hover:bg-gray-100 relative`} // Add relative here
+                  onClick={() => setIsAddMenuVisible(!isAddMenuVisible)}
+                  ref={popupRef}
+                >
+                  <button>
+                    <PlusIcon className="h-12 w-12 ml-0 p-2 bg-grey-50 hover:bg-gray-100 rounded-full" />
+                  </button>
+                  <div
+                    className={`${isMenuToggle ? "ml-3" : "ml-0"} duration-75`}
+                  >
+                    {isMenuToggle ? "Add" : ""}
+                  </div>
+                </div>
+                {isAddMenuVisible && (
+                  <div className="absolute top-36 left-2 w-60 h-32 border rounded-lg bg-white">
+                    {/* menu options... */}
+                    <div className="flex flex-col gap-2 m-4">
+                      <button
+                        className="flex text-md w-full hover:bg-gray-200 p-2"
+                        onClick={() => setIsNewModelWindowVisible(true)}
+                      >
+                        <PuzzlePieceIcon className="h-7 w-7 mr-2" />
+                        New Model
+                      </button>
+                      <button className="flex text-md w-full hover:bg-gray-200 p-2">
+                        <CircleStackIcon className="h-7 w-7 mr-2" />
+                        New Dataset
+                      </button>
+                    </div>
+                  </div>
+                )}
+                {/* MENU ITEMS */}
+                <ul
+                  className="overflow-y-auto overflow-x-hidden"
+                  onMouseEnter={() => {
+                    if (isAboveMedium) {
+                      timeoutId = setTimeout(() => {
+                        setIsMenuToggle(true);
+                      }, 400); // 500ms delay
+                    }
+                  }}
+                  onMouseLeave={() => {
+                    if (isAboveMedium) {
+                      clearTimeout(timeoutId);
+                      setIsMenuToggle(false);
+                    }
+                  }}
+                >
+                  {NavBarData.map((item, index) => {
+                    return (
+                      <Link
+                        to={item.path}
+                        key={index}
+                        onClick={() => {
+                          setSelectedMenuItem(index);
+                        }}
+                      >
+                        <li
+                          className={`${
+                            selectedMenuItem === index
+                              ? "bg-gray-100 border-r-4 border-r-indigo-500"
+                              : "hover:bg-gray-100"
+                          } h-16 flex justify-start items-center hover:bg-gray-100 cursor-pointer duration-75"`}
+                        >
+                          <div className="flex justify-start items-center">
+                            <div className="ml-4">
+                              <item.icon className="h-7 w-7 mr-2" />
+                            </div>
+                            <div
+                              className={`${
+                                isMenuToggle ? "ml-4" : "ml-0"
+                              } text-md`}
+                            >
+                              {isMenuToggle ? item.title : ""}
+                            </div>
+                          </div>
+                        </li>
+                      </Link>
+                    );
+                  })}
+                </ul>
+              </div>
+            )
+            : (
+              <div className={`${flexBetween} pt-4 pl-4`}>
                 <button>
                   <Bars3Icon
                     className="h-12 w-12 mr-4 ml-0 p-2 bg-grey-50 hover:bg-gray-100 rounded-full"
                     onClick={() => setIsMenuToggle(!isMenuToggle)}
                   />
                 </button>
-                <img
-                  src="https://img.logoipsum.com/243.svg"
-                  className={`${isMenuToggle ? "w-32" : "w-0"} duration-75`}
-                />
               </div>
-              {/* ADD BUTTON */}
-              <div
-                className={`${flexBetween} ${
-                  isMenuToggle ? "w-32" : "w-16"
-                } mb-3 p-2 border rounded-full bg-grey-50 hover:bg-gray-100 relative`} // Add relative here
-                onClick={() => setIsAddMenuVisible(!isAddMenuVisible)}
-                ref={popupRef}
-              >
-                <button>
-                  <PlusIcon className="h-12 w-12 ml-0 p-2 bg-grey-50 hover:bg-gray-100 rounded-full" />
-                </button>
-                <div
-                  className={`${isMenuToggle ? "ml-3" : "ml-0"} duration-75`}
-                >
-                  {isMenuToggle ? "Add" : ""}
-                </div>
-              </div>
-              {isAddMenuVisible && (
-                <div className="absolute top-36 left-2 w-60 h-32 border rounded-lg bg-white">
-                  {/* menu options... */}
-                  <div className="flex flex-col gap-2 m-4">
-                    <button className="flex text-md w-full hover:bg-gray-200 p-2" onClick={() => setIsNewModelWindowVisible(true)}>
-                      <PuzzlePieceIcon className="h-7 w-7 mr-2" />
-                      New Model
-                    </button>
-                    <button className="flex text-md w-full hover:bg-gray-200 p-2">
-                      <CircleStackIcon className="h-7 w-7 mr-2" />
-                      New Dataset
-                    </button>
-                  </div>
-                </div>
-              )}
-              {/* MENU ITEMS */}
-              <ul
-                className="overflow-y-auto overflow-x-hidden"
-                onMouseEnter={() => {
-                  if (isAboveMedium) {
-                    timeoutId = setTimeout(() => {
-                      setIsMenuToggle(true);
-                    }, 400); // 500ms delay
-                  }
-                }}
-                onMouseLeave={() => {
-                  if (isAboveMedium) {
-                    clearTimeout(timeoutId);
-                    setIsMenuToggle(false);
-                  }
-                }}
-              >
-                {NavBarData.map((item, index) => {
-                  return (
-                    <Link
-                      to={item.path}
-                      key={index}
-                      onClick={() => {
-                        setSelectedMenuItem(index);
-                      }}
-                    >
-                      <li
-                        className={`${
-                          selectedMenuItem === index
-                            ? "bg-gray-100 border-r-4 border-r-indigo-500"
-                            : "hover:bg-gray-100"
-                        } h-16 flex justify-start items-center hover:bg-gray-100 cursor-pointer duration-75"`}
-                      >
-                        <div className="flex justify-start items-center">
-                          <div className="ml-4">
-                            <item.icon className="h-7 w-7 mr-2" />
-                          </div>
-                          <div
-                            className={`${
-                              isMenuToggle ? "ml-4" : "ml-0"
-                            } text-md`}
-                          >
-                            {isMenuToggle ? item.title : ""}
-                          </div>
-                        </div>
-                      </li>
-                    </Link>
-                  );
-                })}
-              </ul>
-            </div>
-          )
-          : (
-            <div className={`${flexBetween} pt-4 pl-4`}>
-              <button>
-                <Bars3Icon
-                  className="h-12 w-12 mr-4 ml-0 p-2 bg-grey-50 hover:bg-gray-100 rounded-full"
-                  onClick={() => setIsMenuToggle(!isMenuToggle)}
-                />
-              </button>
-            </div>
-          )}
-      </nav>
-    </aside>
+            )}
+        </nav>
+      </aside>
     </div>
   );
 });

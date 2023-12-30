28ce76d81b402c91741c7eb22c4028c748c91136
Vivarium69420 <vivarium69420@gmail.com>
2023-12-30 17:29:06 +0700

    add more auth properties from fetched data

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,37 @@
+import UserDashboardAI from "@/apis/UserDashboardAI";
 import Container from "@/components/container";
 import useAuth from "@/hooks/useAuth";
+import useAxios from "@/hooks/useAxios";
+import { useEffect } from "react";
 
 const Home = () => {
   const { auth } = useAuth();
+  const { setAuth } = useAuth();
+  const [response, error, loading, refetch] = useAxios({
+    axiosInstance: UserDashboardAI,
+    method: "get",
+    url: "/users/profile?username=" + auth?.userName,
+    requestConfig: {
+      headers: {
+        Authorization: `Bearer ${auth?.token}`,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (response.data) {
+      setAuth({
+        token: auth!.token,
+        userName: auth!.userName,
+        role: response.data.roles,
+      });
+ 
+    }
+  }, [response]);
+
   return (
     <Container>
-      <h1 className="text-5xl font-bold">Welcome, {auth?.userName}</h1>
+      <h1 className="text-5xl font-bold">Welcome, {auth?.role} {auth?.userName}</h1>
     </Container>
   );
 };
--- src/screens/user-profile/index.tsx
+++ src/screens/user-profile/index.tsx
@@ -33,6 +33,12 @@ const UserProfile = () => {
     }
   }, [response]);
 
+  useEffect(() => {
+    if (userDetails) {
+      auth!.userId = userDetails.id;
+    }
+  }, []);
+
   const formattedCreateTime = userDetails
     ? new Date(userDetails.createTime).toISOString().split("T")[0]
     : "";
@@ -42,8 +48,9 @@ const UserProfile = () => {
       <div className="flex flex-col items-center justify-center">
         <img src={"https://picsum.photos/200"} alt="" />
         <h1 className="text-2xl font-bold my-2">{auth?.userName}</h1>
-        <span className="flex font-thin items-center justify-center">
+        <span className="flex flex-col font-thin items-center justify-center">
           <p>Joined {formattedCreateTime}</p>
+          <p>Role: {auth?.role}</p>
         </span>
       </div>
     </Container>

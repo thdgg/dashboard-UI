18d254a9aad9613016a5b7683e280c9866bff311
Vivarium69420 <vivarium69420@gmail.com>
2023-12-31 23:40:04 +0700

    template for explorer

---
+++ src/screens/explorer/ExplorerData.ts
@@ -0,0 +1,32 @@
+import { CardProps } from "@/components/model-card";
+
+export const cardData: CardProps[] = [
+  {
+    modelName: "Model 1",
+    author: "Author 1",
+    detail: "Detail 1",
+    description: "Description 1",
+    rating: 5,
+  },
+  {
+    modelName: "Model 1",
+    author: "Author 1",
+    detail: "Detail 1",
+    description: "Description 1",
+    rating: 5,
+  },
+  {
+    modelName: "Model 1",
+    author: "Author 1",
+    detail: "Detail 1",
+    description: "Description 1",
+    rating: 5,
+  },
+  {
+    modelName: "Model 1",
+    author: "Author 1",
+    detail: "Detail 1",
+    description: "Description 1",
+    rating: 5,
+  },
+];
--- src/screens/explorer/index.tsx
+++ src/screens/explorer/index.tsx
@@ -1,9 +1,28 @@
 import Container from "@/components/container";
+import GridComponent from "@/components/model-card";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import { cardData } from "./ExplorerData";
 
 const Explorer = () => {
   return (
     <Container>
       <h1 className="text-5xl font-bold">Explorer</h1>
+      <div className="flex gap-5 mt-5">
+        <button className="flex w-24 h-auto p-3 border-2 rounded-lg gap-5 hover:bg-gray-100">
+          <p>Type</p>
+          <ChevronDownIcon className="w-4 h-4 mt-1" />
+        </button>
+        <button className="flex w-32 h-auto p-3 border-2 rounded-lg gap-5 hover:bg-gray-100">
+          <p>Modified</p>
+          <ChevronDownIcon className="w-4 h-4 mt-1" />
+        </button>
+      </div>
+      <div className="mt-8">
+        <div className="space-y-2">
+          <GridComponent cards={cardData} />
+          <GridComponent cards={cardData} />
+        </div>
+      </div>
     </Container>
   );
 };

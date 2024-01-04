0eb293bc229176fc0a674ab9b282869f7e5a7d84
Vivarium69420 <vivarium69420@gmail.com>
2024-01-04 17:10:00 +0700

    match Models

--- src/screens/explorer/index.tsx
+++ src/screens/explorer/index.tsx
@@ -1,26 +1,27 @@
 import Container from "@/components/container";
 import GridComponent from "@/components/model-card";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
-import { cardData } from "./ExplorerData";
+import { ModelData } from "../models/ModelData";
 
 const Explorer = () => {
   return (
     <Container>
       <h1 className="text-5xl font-bold">Explorer</h1>
-      <div className="flex gap-5 mt-5">
-        <button className="flex w-24 h-auto p-3 border-2 rounded-lg gap-5 hover:bg-gray-100">
-          <p>Type</p>
+      {/* FILTERS */}
+      <div className="flex gap-5 mt-10">
+        <button className="flex w-auto h-auto p-3 border-2 rounded-lg gap-2 hover:bg-gray-100">
+          <p>Star</p>
           <ChevronDownIcon className="w-4 h-4 mt-1" />
         </button>
-        <button className="flex w-32 h-auto p-3 border-2 rounded-lg gap-5 hover:bg-gray-100">
-          <p>Modified</p>
+        <button className="flex w-auto h-auto p-3 border-2 rounded-lg gap-2 hover:bg-gray-100">
+          <p>Inferences</p>
           <ChevronDownIcon className="w-4 h-4 mt-1" />
         </button>
       </div>
       <div className="mt-8">
         <div className="space-y-2">
-          <GridComponent cards={cardData} />
-          <GridComponent cards={cardData} />
+          <GridComponent data={ModelData} />
+          <GridComponent data={ModelData} />
         </div>
       </div>
     </Container>

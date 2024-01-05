c10a785a4762e56fbab7b3ad11571f12b3d7019e
Vivarium69420 <vivarium69420@gmail.com>
2024-01-05 14:32:11 +0700

    signout screen

--- src/screens/signout/index.tsx
+++ src/screens/signout/index.tsx
@@ -1,9 +1,14 @@
 import Container from "@/components/container";
 
 const SignOut = () => {
+  const handleSignOut = () => {
+    localStorage.removeItem("auth");
+    window.location.href = "/";
+  };
   return (
     <Container>
       <h1 className="text-5xl font-bold">SignOut</h1>
+      <button onClick={handleSignOut} className="mt-10 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Ya sure?</button>
     </Container>
   );
 };
